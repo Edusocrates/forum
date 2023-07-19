e259c3f922709b18ba0a2aacb3457dbdd05b3833
edusocrates <edu5390@gmail.com>
2023-07-19 13:26:14 -0300

    Clean Code, removendo comentarios

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -32,12 +32,7 @@ import br.com.alura.forum.repository.TopicoRepository;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
-	
-//	@Autowired
-//	private TopicoRepository topicoRepository;
-//
-//	@Autowired
-//	private CursoRepository cursoRepository;
+
 
 	@Autowired
 	private TopicoService topicoService;
@@ -50,13 +45,6 @@ public class TopicosController {
 										 direction = Sort.Direction.DESC,
 										 page = 0, size = 10) Pageable paginacao) {
 
-//		if (nomeCurso == null) {
-//			Page<Topico> topicos = topicoRepository.findAll(paginacao);
-//			return TopicoDto.converter(topicos);
-//		} else {
-//			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso,paginacao);
-//			return TopicoDto.converter(topicos);
-//		}
 		Page<TopicoDto> lista = topicoService.listar(nomeCurso,paginacao);
 		return lista;
 	}
@@ -65,14 +53,10 @@ public class TopicosController {
 	@Transactional
 	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
-//		Topico topico = form.converter(cursoRepository);
-//		topicoRepository.save(topico);
 		TopicoDto topicoDto = topicoService.cadastrar(form);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topicoDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(topicoDto);
-		
-//		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
-//		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+
 	}
 	
 	@GetMapping("/{id}")
@@ -80,40 +64,22 @@ public class TopicosController {
 		//Optional<Topico> topico = topicoRepository.findById(id);
 		DetalhesDoTopicoDto detalhesDoTopicoDto = topicoService.detalhar(id);
 		return ResponseEntity.ok(detalhesDoTopicoDto);
-//		if (topico.isPresent()) {
-//			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
-//		}
-//
-//		return ResponseEntity.notFound().build();
 	}
 	
 	@PutMapping("/{id}")
 	@Transactional
 //	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) throws NotFoundException {
-		//Optional<Topico> optional = topicoRepository.findById(id);
 		TopicoDto topicoDto = topicoService.atualizar(id,form);
 		return ResponseEntity.ok(topicoDto);
-//		if (optional.isPresent()) {
-//			Topico topico = form.atualizar(id, topicoRepository);
-//			return ResponseEntity.ok(new TopicoDto(topico));
-//		}
-//
-//		return ResponseEntity.notFound().build();
 	}
 	
 	@DeleteMapping("/{id}")
 	@Transactional
 //	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) throws NotFoundException {
-//		Optional<Topico> optional = topicoRepository.findById(id);
-//		if (optional.isPresent()) {
-//			topicoRepository.deleteById(id);
 		topicoService.remover(id);
 		return ResponseEntity.ok().build();
-//		}
-//
-//		return ResponseEntity.notFound().build();
 	}
 
 }
